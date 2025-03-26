bb98fe647a02717511100f617edd81570f80974f
duongnghoang <hoangduongatwork@gmail.com>
2025-03-26 17:26:41 +0700

    add car entity

---
+++ Day1/Entities/Car.cs
@@ -0,0 +1,28 @@
+﻿namespace Day1.Entities
+{
+    public class Car
+    {
+        public Car(string make, string model, int year, CarType type)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Type = type;
+        }
+        public Car() { }
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public int? Year { get; set; }
+        public CarType Type { get; set; }
+        public override string ToString()
+        {
+            return $"Make: {Make}, Model: {Model}, Year: {Year}, Type: {Type}";
+        }
+    }
+
+    public enum CarType
+    {
+        Electric,
+        Fuel,
+    }
+}
