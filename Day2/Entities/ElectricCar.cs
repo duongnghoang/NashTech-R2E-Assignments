7f5828c87861b594990d0af4e2b046a7fb94c2cd
duongnghoang <hoangduongatwork@gmail.com>
2025-03-27 21:33:32 +0700

    abstract car and implementation

---
+++ Day2/Entities/ElectricCar.cs
@@ -0,0 +1,16 @@
+﻿using Day2.Interfaces;
+
+namespace Day2.Entities;
+
+public class ElectricCar : Car, IChargeable
+{
+    public ElectricCar(string make, string model, int year, DateTime lastMaintenanceDate) 
+        : base(make, model, year, lastMaintenanceDate)
+    {
+    }
+
+    public void Charge(DateTime timeOfCharge)
+    {
+        Console.WriteLine($"{GetType().Name} {Make} {Model} recharged on {timeOfCharge:yyyy-MM-dd HH:mm}");
+    }
+}
