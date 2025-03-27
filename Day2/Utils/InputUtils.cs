232ef6eb26b1316d6cd8e45021e6301aef65d9bd
duongnghoang <hoangduongatwork@gmail.com>
2025-03-27 21:34:22 +0700

    input and validations

---
+++ Day2/Utils/InputUtils.cs
@@ -0,0 +1,90 @@
+﻿using Day2.Constants;
+using System.Globalization;
+using Day2.Entities;
+
+namespace Day2.Utils
+{
+    /// <summary>
+    /// Handles input and output operations
+    /// </summary>
+    public static class InputUtils
+    {
+        public static string GetUserStringInput(string message, string invalidMessage)
+        {
+            Console.Write(message);
+            while (true)
+            {
+                string? input = Console.ReadLine()?.Trim();
+                if (ValidationUtils.IsValidString(input))
+                {
+                    return input!;
+                }
+                Console.WriteLine(invalidMessage);
+                Console.Write(message);
+            }
+        }
+
+        public static int GetUserYearInput(string message, string invalidMessage)
+        {
+            Console.Write(message);
+            while (true)
+            {
+                string? input = Console.ReadLine()?.Trim();
+                if (ValidationUtils.IsValidYear(input))
+                {
+                    return int.Parse(input!);
+                }
+                Console.WriteLine(invalidMessage);
+                Console.Write(message);
+            }
+        }
+
+        public static DateTime GetUserDateTimeInput(string message, string dateFormat)
+        {
+            Console.Write(message);
+            while (true)
+            {
+                string? input = Console.ReadLine()?.Trim();
+                if (ValidationUtils.IsValidDate(input, dateFormat))
+                {
+                    return DateTime.ParseExact(input!, dateFormat, 
+                        CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+                }
+                Console.WriteLine(ApplicationMessage.InvalidDateMessage(dateFormat));
+                Console.Write(message);
+            }
+        }
+
+        public static DateTime GetUserDateTimeInput(string message, string dateFormat, 
+            DateTime validStartDate, DateTime validEndDate)
+        {
+            Console.WriteLine(message);
+            while (true)
+            {
+                string? input = Console.ReadLine()?.Trim();
+                if (ValidationUtils.IsValidDate(input, dateFormat, validStartDate, validEndDate))
+                {
+                    return DateTime.ParseExact(input!, dateFormat, 
+                        CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+                }
+                Console.WriteLine(ApplicationMessage.InvalidDateMessage(dateFormat));
+                Console.Write(message);
+            }
+        }
+
+        public static string GetUserChoice(string message, string invalidMessage, List<string> choices)
+        {
+            Console.WriteLine(message);
+            while (true)
+            {
+                string? input = Console.ReadLine()?.Trim().ToUpper();
+                if (ValidationUtils.IsValidChoice(input, choices))
+                {
+                    return input!;
+                }
+                Console.WriteLine(invalidMessage);
+                Console.Write(message);
+            }
+        }
+    }
+}
---
+++ Day2/Utils/ValidationUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Day2.Utils;
+
+public static class ValidationUtils
+{
+    public static bool IsValidString(string? input) 
+        => !string.IsNullOrEmpty(input);
+
+    public static bool IsValidYear(string? input)
+    {
+        var currentYear = DateTime.UtcNow.Year;
+        if (int.TryParse(input, out int year))
+        {
+            return year > 1900 && year <= currentYear;
+        }
+        return false;
+    }
+
+    public static bool IsValidDate(string? input, string dateFormat) 
+        => DateTime.TryParseExact(input, dateFormat, CultureInfo.InvariantCulture, 
+            DateTimeStyles.AssumeUniversal, out var date)
+            && DateTime.Compare(DateTime.UtcNow, date) >= 0;
+
+    public static bool IsValidDate(string? input, string dateFormat, 
+        DateTime validStartDate, DateTime validEndDate)
+        => DateTime.TryParseExact(input, dateFormat, CultureInfo.InvariantCulture, 
+               DateTimeStyles.AssumeUniversal, out DateTime date)
+            && DateTime.Compare(validStartDate, date) <= 0 
+            && DateTime.Compare(validEndDate, date) >= 0;
+
+    public static bool IsValidChoice(string? input, List<string> choice) 
+        => !string.IsNullOrEmpty(input) && choice.Contains(input);
+}
