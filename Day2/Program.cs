eb2894e4f5ca84ba6a7c04a2bb6f07634c644d15
duongnghoang <hoangduongatwork@gmail.com>
2025-03-27 21:34:33 +0700

    main program

---
+++ Day2/Program.cs
@@ -0,0 +1,16 @@
+﻿using Day2.Constants;
+using Day2.Entities;
+using Day2.Services;
+using Day2.Utils;
+
+namespace Day2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CarService carService = new CarService(); 
+        var car = carService.GetUserCar();
+        carService.RefuelOrChargeCar(car);
+    }
+}
